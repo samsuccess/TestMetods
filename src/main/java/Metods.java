a6839724cea9485a4bdfc85c50a3ef577e342eda
samsuccess <samsuccess36@gmail.com>
2020-11-09 21:23:07 +0300

    final homework6

--- src/main/java/Metods.java
+++ src/main/java/Metods.java
@@ -2,30 +2,46 @@ import java.util.Arrays;
 
 public class Metods {
 
-    public static int[] afterFour(int[] arr) throws RuntimeException {
-        int n = 0;
-        String str = Arrays.toString(arr);
-        if (str.indexOf(String.valueOf(4)) != -1) {
-            for (int i = 0; i < arr.length; i++) {
+//    public static int[] afterFour(int[] arr) throws RuntimeException {
+//        int n = 0;
+//        String str = Arrays.toString(arr);
+//        if (str.indexOf(String.valueOf(4)) != -1) {
+//            for (int i = 0; i < arr.length; i++) {
+//                if (arr[i] == 4) {
+//                    n = i;
+//                }
+//            }
+//            int[] arr1 = new int[arr.length - n - 1];
+//            System.arraycopy(arr, n + 1, arr1, 0, arr1.length);
+//            return arr1;
+//        } else {
+//            throw new RuntimeException("4 не найдена");
+//        }
+//    }
+
+    public static int[] afterFour(int[] arr) {
+        for (int i = arr.length -1; i >= 0; i--) {
                 if (arr[i] == 4) {
-                    n = i;
+                    return Arrays.copyOfRange(arr, i +1, arr.length);
                 }
-            }
-            int[] arr1 = new int[arr.length - n - 1];
-            System.arraycopy(arr, n + 1, arr1, 0, arr1.length);
-            return arr1;
-        } else {
-            throw new RuntimeException("4 не найдена");
         }
+        throw new RuntimeException("4 не найдена");
     }
 
     public static boolean oneAndFour(int[] arr) {
-        boolean i1 = false, i4 = false;
+        boolean i1 = false;
+        boolean i4 = false;
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 1) i1 = true;
-            else if (arr[i] == 4) i4 = true;
-            else return false;
-        }
+            if(arr[i] !=1 && arr[i] != 4) {
+                return false;
+            }
+            if (arr[i] == 1) {
+                i1 = true;
+            }
+            if (arr[i] == 4) {
+                i4 = true;
+                }
+            }
         return i1 && i4;
     }
 
---
+++ src/test/java/TestAll.java
@@ -0,0 +1,10 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({TestMetod1.class, TestMetod2.class})
+
+public class TestAll {
+
+}
